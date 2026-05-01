b3e559f3985b026dc4f3a4290e48e78d64b4a727
root <root@e2b.local>
2026-05-01 21:28:27 +0000

    fix: stripe apiVersion type error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2024-09-30.acacia',
+  apiVersion: '2024-06-20' as any,
 });
 
 export const STRIPE_PLANS: Record<string, string> = {
